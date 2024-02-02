d91fa9605d0880bcdf1602da8bb831d178bcd1a1
shalinder kumawat <kumarkumawatkumarkumawat@gmail.com>
2024-02-02 13:55:03 +0530

    6 value add

---
+++ q5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+ int main()
+{
+	int a,b,c,d,e,f;
+	printf("enter the value");
+	scanf("%d\n%d\n%d\n%d\n%d\n%d\n",&a,&b,&c,&d,&e,&f);
+	int o=a+b+c+d+e+f;
+	printf("%d", o);
+	return 0;
+}
